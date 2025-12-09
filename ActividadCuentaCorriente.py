9bc16c98dce9d4d2edf2bfb5e9eeda7fe4d897d1
Mario Martin <147378155+mariomm2@users.noreply.github.com>
2025-12-09 11:52:57 +0100

    Actualizacion 9Dic2025

--- ActividadCuentaCorriente.py
+++ ActividadCuentaCorriente.py
@@ -1,5 +1,5 @@
 
-def calcular_dc1(entidad, sucursal):
+def calcular_primer_dig(entidad, sucursal):
     pesos_entidad = [4, 8, 5, 10]
     pesos_sucursal = [9, 7, 3, 6]
     suma = 0
@@ -21,14 +21,14 @@ def calcular_dc1(entidad, sucursal):
     return digito
 
 
-def calcular_dc2(numero):
+def calcular_segundo_dig(numero):
     pesos = [1, 2, 4, 8, 5, 10, 9, 7, 3, 6]
     suma = 0
 
     for i in range(10):
         suma += int(numero[i]) * pesos[i]
 
-    resto = 11 - (suma % 11)
+    resto = 11-(suma % 11)
 
     digito = resto
     if resto == 11:
@@ -50,8 +50,8 @@ def validar_ccc(cuenta):
         dc2 = int(cuenta[9])
         numero = cuenta[10:]
 
-        dc1_calc = calcular_dc1(entidad, sucursal)
-        dc2_calc = calcular_dc2(numero)
+        dc1_calc = calcular_primer_dig(entidad, sucursal)
+        dc2_calc = calcular_segundo_dig(numero)
 
         if dc1 == dc1_calc and dc2 == dc2_calc:
             es_valida = True
